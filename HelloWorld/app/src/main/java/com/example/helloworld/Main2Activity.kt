6b9a808f8c1dbfca287a985be4ef8d55ed38e6ba
willymello <willy.mello@cantinaconsulting.com>
2019-08-16 14:01:07 -0400

    day dos

--- HelloWorld/app/src/main/java/com/example/helloworld/Main2Activity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/Main2Activity.kt
@@ -1,12 +1,28 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        //var~=let in js
+        //val~= const in js
+        val greetingText = findViewById<TextView>(R.id.greeting_text)
+        val personName= findViewById<EditText>(R.id.person_name)
+        val greetButton= findViewById<Button>(R.id.action_greet)
+        val farewellButton= findViewById<Button>(R.id.action_goodbye)
+
+        greetButton.setOnClickListener{btn -> greetingText.text="Hello ${personName.text}"}
+
+        farewellButton.setOnClickListener{btn -> greetingText.text="Get out of my face ${personName.text}" }
+
+
     }
+
 }
